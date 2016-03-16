ada8bde029e436b4e6a1b46deb39d1380dd43313
liuweichao <270253535@qq.com>
2016-03-16 17:43:10 +0800

    豆瓣api同步

--- public/admin.js
+++ public/admin.js
@@ -13,5 +13,28 @@ $(function(){
 				}
 			}
 		})
+	});
+
+	$('#Douban').blur(function(){
+		var douban = $(this);
+		var id = douban.val();
+
+		$.ajax({
+			type: "get",
+			url: "https://api.douban.com/v2/movie/subject/"+ id,
+			dataType: "jsonp",
+			cache:true,
+			crossDomain:true,
+			jsonp:'callback',
+			success: function(data){
+				$("#inputTitle").val(data.title);
+				$("#inputDoctor").val(data.casts[0].name);
+				$("#inputCountry").val(data.countries.join(""));
+				$("#inputPoster").val(data.images.large);
+				$("#inputYear").val(data.year);
+				$("#inputSummary").val(data.summary);
+			}
+		});
 	})
+
 });
