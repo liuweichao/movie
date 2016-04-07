5b3698058a94ee3f547b8f3c54f1dde1d59e356b
liuweichao <270253535@qq.com>
2016-04-07 15:02:02 +0800

    pv统计

--- app/controllers/movie.js
+++ app/controllers/movie.js
@@ -9,6 +9,11 @@ var path = require('path');
 exports.detail = function(req,res){
     var id = req.params.id;
 
+    Movie.update({_id: id}, {$inc: {pv: 1}},function(err){
+        if(err){
+            console.log(err);
+        }
+    });
     Movie.findById(id,function(err,movie){
         Comment.
             find({movie: id})
